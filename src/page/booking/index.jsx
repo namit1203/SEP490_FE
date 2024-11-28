897a44977b8a27e6ac0f3b2fc45b7696963a00d2
Phạm Đức Bình <binhpd@oeg.vn>
2024-11-28 16:04:28 +0700

    update navbooking

---
+++ src/page/booking/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBooking from "./modules/NavBooking";
+export default function BookingCarViews() {
+  return (
+    <div className="max-w-[1036px] mx-auto pt-5">
+      <div className="bg-white border border-gray-50 rounded-lg shadow h-36">
+        <NavBooking />
+      </div>
+    </div>
+  );
+}
