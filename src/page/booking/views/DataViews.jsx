04fee09546a467f8d1969be9975f2654c09904d4
Phạm Đức Bình <binhpd@oeg.vn>
2024-11-29 09:18:48 +0700

    update

--- src/page/booking/views/DataViews.jsx
+++ src/page/booking/views/DataViews.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BookingSort from "./sort/BookingSort";
 
 export default function DataViews() {
   const sortingOptions = [
@@ -12,24 +13,19 @@ export default function DataViews() {
 
   return (
     <div className="flex w-full gap-2.5">
-      <div className="flex flex-col gap-4 overflow-hidden w-[266px] border p-4 rounded-lg border-solid border-[rgb(224,224,224)]">
-        <div className="w-full gap-4 flex flex-col">
-          <p className="font-bold text-lg leading-6 mb-0">Sắp xếp</p>
+      <div className="flex flex-col gap-4 overflow-hidden w-[266px]">
+        {/* sort */}
+        <div className="border p-4 rounded-lg border-solid border-[rgb(224,224,224)] flex flex-col gap-4">
+          <BookingSort options={sortingOptions} />
         </div>
-
-        <div className="flex flex-col gap-4">
-          {sortingOptions.map((option) => (
-            <label key={option.id} className="flex items-center gap-2">
-              <input
-                type="radio"
-                name="sorting"
-                className="w-5 h-5 accent-blue-600 cursor-pointer"
-              />
-              <span className="text-sm leading-5 font-semibold">
-                {option.label}
-              </span>
-            </label>
-          ))}
+        {/* filter */}
+        <div className="border p-4 rounded-lg border-solid border-[rgb(224,224,224)] flex flex-col gap-4">
+          <div className="w-full gap-4 flex justify-between">
+            <p className="font-bold text-lg leading-6 mb-0">Lọc</p>
+            <p className="text-sm font-bold leading-5 tracking-[0px] underline underline-offset-1 break-normal mb-0 text-[rgb(36,116,229)] cursor-pointer">
+              Xóa lọc
+            </p>
+          </div>
         </div>
       </div>
     </div>
