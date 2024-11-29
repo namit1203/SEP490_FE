cae379950eaa1c17e47ec4706eb0003a776bc993
Phạm Đức Bình <binhpd@oeg.vn>
2024-11-29 12:53:44 +0700

    update setup redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./stores/store.js";
+import App from "./App.jsx";
 import { AppProvider } from "./context/app.context.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
