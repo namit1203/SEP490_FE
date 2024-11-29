0729c56d49c2fbf25cab8a5f8d313384d6b6b95e
Phạm Đức Bình <binhpd@oeg.vn>
2024-11-29 10:31:46 +0700

    update data filter

--- src/page/BookingCar.jsx
+++ src/page/BookingCar.jsx
@@ -6,7 +6,7 @@ export default function BookingCar() {
   return (
     <>
       <Header />
-      <div className="w-[1036px] flex flex-col gap-5 m-auto pr-2.5 pt-5">
+      <div className="w-[1036px] flex flex-col gap-5 m-auto pr-2.5 pt-5 pb-5">
         <BookingCarViews />
       </div>
       <Footer />
--- src/page/booking/views/DataViews.jsx
+++ src/page/booking/views/DataViews.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DataFilter from "./data/DataFilter";
 import BookingFilter from "./filter/BookingFilter";
 import BookingSort from "./sort/BookingSort";
 
@@ -13,7 +14,8 @@ export default function DataViews() {
   ];
 
   return (
-    <div className="flex w-full gap-2.5">
+    <div className="flex w-full gap-2.5 justify-between">
+      {/* filter and sort */}
       <div className="flex flex-col gap-4 overflow-hidden w-[266px]">
         {/* sort */}
         <div className="border p-4 rounded-lg border-solid border-[rgb(224,224,224)] flex flex-col gap-4">
@@ -23,6 +25,19 @@ export default function DataViews() {
         <div className="border p-4 rounded-lg border-solid border-[rgb(224,224,224)] flex flex-col gap-4">
           <BookingFilter />
         </div>
+      </div>
+
+      {/* data */}
+      <div className="w-[730px] flex flex-col gap-5">
+        <div className="flex gap-2">
+          <p className="text-2xl font-bold leading-8 tracking-[0px] mb-0">
+            Kết quả:
+          </p>
+          <span className="font-bold text-2xl leading-8 tracking-[-0.5px]">
+            170 chuyến
+          </span>
+        </div>
+        <DataFilter />
       </div>
     </div>
   );
